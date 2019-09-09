96fa64af4cd89fd4fb16cacde521ffd118fa46ea
Guciio <gucio332@gmail.com>
2019-09-09 19:13:57 +0200

    S3 and send message to sqs

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -1,10 +1,78 @@
 class IndicesController < ApplicationController
-  before_action :set_index, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token
   # GET /indices
   # GET /indices.json
   def index
+    get_photos
     @indices = Index.all
+  end
+
+  def rotate_photo
+    sqs = Aws::SQS::Client.new(
+        region: Rails.application.credentials.aws[:aws_region],
+        access_key_id: Rails.application.credentials.aws[:access_key_id],
+        secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    sqs.send_message(queue_url: 'https://sqs.us-west-2.amazonaws.com/801463284499/awsprojectqueue.fifo',
+                     message_body: "rotate: " + params[:choosen_photo].to_s,
+                     message_group_id: rand(1..100).to_s)
+    redirect_to action: "index"
+  end
+
+  def send_mesage
+    sqs = Aws::SQS::Client.new(
+        region: Rails.application.credentials.aws[:aws_region],
+        access_key_id: Rails.application.credentials.aws[:access_key_id],
+        secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    # entries = Array.new(params[:keys].size)
+    # i=0
+    # params[:keys].each do |key|
+    #   body = {
+    #       key: key,
+    #       operation: params[:operation],
+    #       value: params[:value]
+    #   }
+    #   obj = {
+    #       id: 'msg' + i.to_s,
+    #       message_body: JSON[body].to_s
+    #   }
+    #   entries[i] = obj
+    #   i += 1
+    # end
+    sqs.send_message(queue_url: 'https://sqs.us-west-2.amazonaws.com/801463284499/awsprojectqueue.fifo', message_body: params[:message],message_group_id: "1")
+    redirect_to action: "index"
+  end
+
+  def delete_photo
+    s3 = Aws::S3::Resource.new(region: Rails.application.credentials.aws[:aws_region],
+                               access_key_id: Rails.application.credentials.aws[:access_key_id],
+                               secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    s3.bucket('awsprojectbuckett').delete_objects({
+        delete: {
+            objects: [
+                {
+                    key: params[:choosen_photo].to_s
+                },
+            ],
+        },
+                                                  })
+    redirect_to action: "index"
+  end
+
+  def get_photos
+    s3 = Aws::S3::Resource.new(region: Rails.application.credentials.aws[:aws_region],
+                               access_key_id: Rails.application.credentials.aws[:access_key_id],
+                               secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    obj = s3.bucket('awsprojectbuckett').objects
+    @readedPhoto = obj
+  end
+
+  def send_photo
+    s3 = Aws::S3::Resource.new(region: Rails.application.credentials.aws[:aws_region],
+                               access_key_id: Rails.application.credentials.aws[:access_key_id],
+                               secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    obj = s3.bucket('awsprojectbuckett').object(params[:photo])
+    obj.upload_file('/home/binarsummer/Desktop/'+ params[:photo])
+    redirect_to action: "index"
   end
 
   # GET /indices/1
@@ -71,4 +139,5 @@ class IndicesController < ApplicationController
     def index_params
       params.fetch(:index, {})
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :indices
+  get 'rotatephoto', to: 'indices#rotate_photo'
+  get 'sendphoto', to: 'indices#send_photo'
+  get 'deletephoto', to: 'indices#delete_photo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
